7638d7ff218558f9ff6635ac9266e47111bf5d84
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-04-28 16:13:23 +0200

    change loader color

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,7 @@ import { BiLoaderCircle } from "react-icons/bi";
 const Loader = () => {
   return (
     <div className="absolute inset-0 flex justify-center items-start bg-black bg-opacity-20">
-      <div className="flex flex-col items-center bg-blue-600 p-4 rounded-md mt-32">
+      <div className="flex flex-col items-center bg-indigo-700 py-2 px-4 rounded-md mt-32">
         <BiLoaderCircle className="text-4xl text-white mb-2 animate-spin-slow" />
         <p className="text-white font-semibold">Loading Data..</p>
       </div>
