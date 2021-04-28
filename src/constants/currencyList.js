26409557aa27d9a93ea9a5147ccebc929d32ac1f
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-04-28 11:31:33 +0200

    add list of available coins & currencies to constants

---
+++ src/constants/currencyList.js
@@ -0,0 +1,10 @@
+const currency = [
+  { id: "usd", name: "USD", symbol: "$" },
+  { id: "eur", name: "EUR", symbol: "€" },
+  { id: "gbp", name: "GBP", symbol: "£" },
+  { id: "aud", name: "AUD", symbol: "$" },
+  { id: "cad", name: "CAD", symbol: "$" },
+  { id: "cny", name: "CNY", symbol: "¥" },
+  { id: "jpy", name: "JPY", symbol: "¥" },
+];
+export default currency;
