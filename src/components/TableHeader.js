54b54104252be33fbbdad5eff6ae55fbc33a4289
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-04-30 18:50:03 +0200

    add caret sorting indicators to table headings

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setSorting } from "../actions/CoinActions";
 import headings from "../constants/tableHeadings";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 
 const TableHeader = () => {
   const sorting = useSelector((state) => state.sorting);
@@ -13,11 +14,17 @@ const TableHeader = () => {
         <p
           key={heading.key}
           onClick={() => dispatch(setSorting(heading.key, !sorting.order))}
-          className={`${i === 3 && "hidden lg:block"} ${
-            i === 4 && "hidden md:block"
-          }`}
+          className={`flex items-center cursor-pointer ${
+            i === 3 && "hidden lg:flex"
+          } ${i === 4 && "hidden md:flex "}`}
         >
           {heading.name}
+          {sorting.heading === heading.key && !sorting.order && (
+            <AiFillCaretDown className="ml-1 text-xs" />
+          )}
+          {sorting.heading === heading.key && sorting.order && (
+            <AiFillCaretUp className="ml-1 text-xs" />
+          )}
         </p>
       ))}
     </div>
