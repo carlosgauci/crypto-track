ba4c58ed58493079228da617ecf9b3ed7ab9fcc6
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-19 21:46:46 +0200

    add bg color to header and combine it with coin list section

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="container text-center text-4xl p-4 mb-2">
-      <h1 className="text-white font-semibold leading-none">Crypto Track</h1>
+    <header className="container text-center text-4xl p-4  bg-gray-900 md:rounded-t-md md:mt-4 ">
+      <h1 className="text-white font-semibold">Crypto Track</h1>
     </header>
   );
 };
